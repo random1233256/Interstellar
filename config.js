20f751faa48dd6bf4e928f93e62e9bba70f74851
random1233256 <160122733+random1233256@users.noreply.github.com>
2024-02-15 04:33:35 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 const config = {
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
-  challenge: false,
+  challenge: true,
   users: {
     // username: 'password', you can add multiple users.
     interstellar: 'password',
+    test: 'testing123',
   },
 }
 export default config
